f549a6178e00f99aad7ce87980285a5d568790ac
Son Vu <truongson.vu72@gmail.com>
2024-07-30 14:52:44 -0400

    Initial commit

---
+++ src/component/page/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import Header from "../../section/HomePage/Header";
+
+export default function HomePage() {
+    return (
+        <div>
+            <Header />
+            
+        </div>
+    )
+}
