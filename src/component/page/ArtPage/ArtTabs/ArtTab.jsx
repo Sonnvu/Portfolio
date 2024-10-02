52d5bca7a648e0d6b07e733a48c0546e1a1f0e5b
Son Vu <truongson.vu72@gmail.com>
2024-10-02 08:36:27 -0400

    Update Art

--- src/component/page/ArtPage/ArtTabs/ArtTab.jsx
+++ src/component/page/ArtPage/ArtTabs/ArtTab.jsx
@@ -3,10 +3,16 @@ import pose2 from './img/pose2.jpg'
 import pman from './img/portraitman.JPG'
 import pwoman from './img/portraitwoman.jpg'
 import geto from './img/geto.jpg'
+import still from './img/still.jpg'
+import hallway from './img/hallway.jpg'
+import bottles from './img/bottles.jpg'
 
 export default function ArtTab() {
     return (
         <div className='grid grid-cols-2 gap-10 mx-32 px-52 pt-20'>
+            <img src={hallway} alt=""/>
+            <img src={still} alt=""/>
+            <img src={bottles} alt=""/>
             <img src={geto} alt=""/>
             <img src={pwoman} alt=""/>
             <img src={pman} alt=""/>
